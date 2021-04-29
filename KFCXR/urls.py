58236fd193a753cd1ef46641a71b391eeb35d423
JonathanCor <83401782+JonathanCor@users.noreply.github.com>
2021-04-29 12:53:22 +0200

    update urls

--- KFCXR/urls.py
+++ KFCXR/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('fifty_shades_of_kfc.urls')),
+    path('FiftyShadesOfKFC/', include('fifty_shades_of_kfc.urls')),
     path('admin/', admin.site.urls),
 ]
