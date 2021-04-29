3497c08c90fe9341b8fe8f1a3a54d68fc01827bf
JonathanCor <83401782+JonathanCor@users.noreply.github.com>
2021-04-29 14:03:28 +0200

    hide secret key

--- KFCXR/settings.py
+++ KFCXR/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%he9=$m_&cof7ffqq&%n^!y!pdct-1-9y2+qp5a9qpboko^!h2'
+env = environ.Env()
+environ.Env.read_env()
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['xrleuven.pythonanywhere.com']
+ALLOWED_HOSTS = ['xrleuven.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -118,6 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 
